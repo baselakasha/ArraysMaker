69e415d76018bc150a051aadb7d589fab7289b77
BaselAkasha <Contact@BaselAkasha.com>
2019-12-30 18:19:58 +0000

    add the script and .gitignore

---
+++ script/script.js
@@ -0,0 +1,58 @@
+class app {
+    constructor(elements) {
+        this.text = elements.text.value;
+        this.separator = elements.separator.value;
+        this.arr_open = elements.arr_open.value;
+        this.arr_close = elements.arr_close.value; 
+        this.warpper =  elements.warpper.value;
+    }
+    make() {
+        let items;
+        if(this.separator == "\\n"){
+            items = this.text.split(/\n/g);
+        }
+        else{
+            items = this.text.replace(/\n/g,"").split(this.separator);
+        }
+
+        let array_text = this.arr_open; 
+        for(let i = 0, l = items.length; i < l; i++){
+            array_text += this.warpper + items[i] + this.warpper;
+            if(!(i == l - 1)){
+                array_text += ", "
+            }
+            else{
+                array_text += this.arr_close;
+            }
+        }
+        return array_text;
+    }
+}
+
+window.onload=function(){
+    let elements = {
+        text : document.getElementById("text"),
+        separator : document.getElementById("separator"),
+        arr_open : document.getElementById("arr_open"),
+        arr_close : document.getElementById("arr_close"),
+        warpper : document.getElementById("warpper"),
+        btn : document.getElementById("make_btn"),
+        output : document.getElementById("array_output"),
+        reset : function(){
+            this.text.innerHTML = "";
+            this.output.innerHTML = "";
+        }
+    }
+    // Empty the text areas when use clicks for the first time
+    let firstTime = true;
+    elements.text.onclick = function(){
+        if(firstTime){
+            elements.reset();
+            firstTime = false;
+        }
+    }
+    elements.btn.onclick = function(){
+        let newApp = new app(elements);
+        elements.output.value = newApp.make();
+    };
+}
