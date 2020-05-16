8225f58c9bbcfe7c6e55d766cd51650775bc9a03
BaselAkasha <Contact@BaselAkasha.com>
2020-05-16 16:04:26 +0100

    refactor the js code

--- script/script.js
+++ script/script.js
@@ -1,59 +1,55 @@
-class app {
-    constructor(elements) {
-        this.text = elements.text.value;
-        this.separator = elements.separator.value;
-        this.arr_open = elements.arr_open.value;
-        this.arr_close = elements.arr_close.value; 
-        this.wrapper =  elements.wrapper.value;
-    }
-    getItems(){
-        if(this.separator == "\\n"){
-            return this.text.split(/\n/g);
-        }
-        else{
-            return this.text.replace(/\n/g,"").split(this.separator);
-        }
-    }
-    make() {
-        let items = this.getItems();
-        let array_text = this.arr_open; 
-        for(let i = 0, l = items.length; i < l; i++){
-            array_text += this.wrapper + items[i] + this.wrapper;
-            if(!(i == l - 1)){
-                array_text += ", "
-            }
-            else{
-                array_text += this.arr_close;
-            }
-        }
-        return array_text;
-    }
-}
+'use strict';
 
 window.onload=function(){
-    let elements = {
-        text : document.getElementById("text"),
-        separator : document.getElementById("separator"),
-        arr_open : document.getElementById("arr_open"),
-        arr_close : document.getElementById("arr_close"),
-        wrapper : document.getElementById("wrapper"),
-        btn : document.getElementById("make_btn"),
-        output : document.getElementById("array_output"),
-        reset : function(){
-            this.text.innerHTML = "";
-            this.output.innerHTML = "";
-        }
-    }
+    // HTML DOM elements
+    const
+        text_input = document.getElementById("text"),
+        separator = document.getElementById("separator"),
+        arr_open = document.getElementById("arr_open"),
+        arr_close = document.getElementById("arr_close"),
+        wrapper = document.getElementById("wrapper"),
+        make_btn = document.getElementById("make_btn"),
+        output = document.getElementById("array_output");
+
     // Empty the text areas when use clicks for the first time
     let firstTime = true;
-    elements.text.onclick = function(){
+    text_input.onclick = function(){
         if(firstTime){
-            elements.reset();
+            resetInputOutput(text_input, output)
             firstTime = false;
         }
     }
-    elements.btn.onclick = function(){
-        let newApp = new app(elements);
-        elements.output.value = newApp.make();
+
+    make_btn.onclick = function(){
+        const items_array = splitItems(text_input.value, separator.value); 
+        output.value = makeInlineOutput(items_array, wrapper.value, arr_open.value, arr_close.value);
     };
+}
+
+function resetInputOutput(input, output){
+    input.innerHTML = "";
+    output.innerHTML = "";
+}
+
+function splitItems(text, separator){
+    if(separator == "\\n"){
+        return text.split(/\n/g);
+    }
+    else{
+        return text.replace(/\n/g,"").split(separator);
+    }
+}
+
+function makeInlineOutput(items_array, wrapper, array_open, array_close){
+    let output = array_open;
+    for(let item = 0, items_length = items_array.length; item < items_length; item++){
+        output += wrapper + items_array[item] + wrapper;
+        if(item != (items_length - 1)){ // Not the last item
+            output += ", "
+        }
+        else{
+            output += array_close;
+        }
+    }
+    return output;
 }
